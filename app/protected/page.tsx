35f9d73245aaece8dce6d47b95b2692abb6842b7
Mauricio Moura <mauriciomourag@gmail.com>
2023-12-28 15:42:58 -0300

    Criacao pagina protegida

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -1,18 +1,157 @@
 import { signOut } from '@/auth';
+import next from 'next';
+import React from 'react';
 
-export default function Protected() {
+// export default function Protected() {
+// //   return (
+// //     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+// //       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+// //         <h1>Área Protegida</h1>
+// //         <div className="container py-5 px-5 mx-0 min-w-full flex flex-col items-center">
+//           <form action={async () => { 'use server'; await signOut(); }} method="post">
+//             <button className="m-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+//         Logout
+//   </button>
+//   </form>
+// </div>
+//     </div>
+//    </main>
+//  )
+//  }
+
+
+
+const HeadlineCards = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <h1>Área Protegida</h1>
-        <div className="container py-5 px-5 mx-0 min-w-full flex flex-col items-center">
-          <form action={async () => { 'use server'; await signOut(); }} method="post">
-            <button className="m-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-              Logout
-            </button>
-          </form>
+    <div className='max-w-[1640px] mx-auto p-4 py-12 grid md:grid-cols-3 gap-6'>
+      {/* Card */}
+      <div className='rounded-xl relative'>
+        {/* Overlay */}
+        <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
+          <p className='font-bold text-2xl px-2 pt-4'>Melhores Obras Cyberpunk</p>
+          <button className='border-white bg-white text-black mx-2 absolute bottom-2 font-bold py-1 px-3 rounded'>Editar</button>
         </div>
+        <img
+        className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl'
+          src='https://images.unsplash.com/photo-1561344640-2453889cde5b?q=80&w=3799&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+          alt='/'
+        />
       </div>
-    </main>
-  )
-}
+      {/* Card */}
+      <div className='rounded-xl relative'>
+        {/* Overlay */}
+        <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
+          <p className='font-bold text-2xl px-2 pt-4'>Melhores filmes de fantasia</p>
+          <p className='px-2'>Added Daily</p>
+          <button className='border-white bg-white text-black mx-2 absolute bottom-2 font-bold py-1 px-3 rounded'>Editar</button>
+        </div>
+        <img
+        className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl'
+          src='https://images.unsplash.com/photo-1535666669445-e8c15cd2e7d9?q=80&w=3593&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+          alt='/'
+        />
+      </div>
+      {/* Card */}
+      <div className='rounded-xl relative'>
+        {/* Overlay */}
+        <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
+          <p className='font-bold text-2xl px-2 pt-4'>Top 10 filmes</p>
+          <button className='border-white bg-white text-black mx-2 absolute bottom-2 font-bold py-1 px-3 rounded'>Editar</button>
+        </div>
+        <img
+        className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl'
+          src='https://images.unsplash.com/photo-1542204165-65bf26472b9b?q=80&w=3774&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+          alt='/'
+        />
+      </div>
+      {/* Card */}
+      <div className='rounded-xl relative'>
+        {/* Overlay */}
+        <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
+          <p className='font-bold text-2xl px-2 pt-4'>Top 10 melhores músicas</p>
+          <button className='border-white bg-white text-black mx-2 absolute bottom-2 font-bold py-1 px-3 rounded'>Editar</button>
+        </div>
+        <img
+        className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl'
+          src='https://images.unsplash.com/photo-1510915361894-db8b60106cb1?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+          alt='/'
+        />
+      </div>
+      {/* Card */}
+      <div className='rounded-xl relative'>
+        {/* Overlay */}
+        <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
+          <p className='font-bold text-2xl px-2 pt-4'>Filmes de terror mais assustadores</p>
+          <button className='border-white bg-white text-black mx-2 absolute bottom-2 font-bold py-1 px-3 rounded'>Editar</button>
+        </div>
+        <img
+        className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl'
+          src='https://images.unsplash.com/photo-1621693733036-e00dc554e19e?q=80&w=3811&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+          alt='/'
+        />
+      </div>
+      {/* Card */}
+      <div className='rounded-xl relative'>
+        {/* Overlay */}
+        <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
+          <p className='font-bold text-2xl px-2 pt-4'>Países mais bonitos</p>
+          <button className='border-white bg-white text-black mx-2 absolute bottom-2 font-bold py-1 px-3 rounded'>Editar</button>
+        </div>
+        <img
+        className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl'
+          src='https://images.unsplash.com/photo-1604549944235-3e5579b15cc2?q=80&w=3871&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+          alt='/'
+        />
+      </div>
+      {/* Card */}
+      <div className='rounded-xl relative'>
+        {/* Overlay */}
+        <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
+          <p className='font-bold text-2xl px-2 pt-4'>Meus 5 jogos favoritos</p>
+          <button className='border-white bg-white text-black mx-2 absolute bottom-2 font-bold py-1 px-3 rounded'>Editar</button>
+        </div>
+        <img
+        className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl'
+          src='https://images.unsplash.com/photo-1493711662062-fa541adb3fc8?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+          alt='/'
+        />
+      </div>
+      {/* Card */}
+      <div className='rounded-xl relative'>
+        {/* Overlay */}
+        <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
+          <p className='font-bold text-2xl px-2 pt-4'>Melhores comidas do mundo</p>
+          <button className='border-white bg-white text-black mx-2 absolute bottom-2 font-bold py-1 px-3 rounded'>Editar</button>
+        </div>
+        <img
+        className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl'
+          src='https://images.unsplash.com/photo-1548940740-204726a19be3?q=80&w=2592&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+          alt='/'
+        />
+      </div>
+      {/* Card */}
+      <div className='rounded-xl relative'>
+        {/* Overlay */}
+        <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
+          <p className='font-bold text-2xl px-2 pt-4'>Top 5 filmes de heróis</p>
+          <button className='border-white bg-white text-black mx-2 absolute bottom-2 font-bold py-1 px-3 rounded'>Editar</button>
+        </div>
+        <img
+        className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl'
+          src='https://images.unsplash.com/photo-1612036782180-6f0b6cd846fe?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+          alt='/'
+        />
+      </div>
+      <form action={async () => { 'use server'; await signOut(); }} method="post">
+            <button className="m-2 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+        Logout
+  </button>
+  </form>
+    </div>
+    
+    
+    
+  );
+};
+
+export default HeadlineCards;
