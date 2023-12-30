3b712b250603ecf51cfedfe492d59cc53c16f1aa
Mauricio Moura <mauriciomourag@gmail.com>
2023-12-30 00:43:28 -0300

    Mudancas Finais

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -1,25 +1,5 @@
 import { signOut } from '@/auth';
-import next from 'next';
 import React from 'react';
-
-// export default function Protected() {
-// //   return (
-// //     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-// //       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-// //         <h1>Área Protegida</h1>
-// //         <div className="container py-5 px-5 mx-0 min-w-full flex flex-col items-center">
-//           <form action={async () => { 'use server'; await signOut(); }} method="post">
-//             <button className="m-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-//         Logout
-//   </button>
-//   </form>
-// </div>
-//     </div>
-//    </main>
-//  )
-//  }
-
-
 
 const HeadlineCards = () => {
   return (
@@ -42,7 +22,6 @@ const HeadlineCards = () => {
         {/* Overlay */}
         <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
           <p className='font-bold text-2xl px-2 pt-4'>Melhores filmes de fantasia</p>
-          <p className='px-2'>Added Daily</p>
           <button className='border-white bg-white text-black mx-2 absolute bottom-2 font-bold py-1 px-3 rounded'>Editar</button>
         </div>
         <img
